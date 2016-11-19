77422d7b60cfdde0b610bb666e5c6736570c1ca2
Filipe Correia <1150524@isep.ipp.pt>
2016-11-19 19:03:33 +0000

    Alterações ao exercicio 10

--- modulo4/ex10a/count_bits_zero.c
+++ modulo4/ex10a/count_bits_zero.c
@@ -2,17 +2,17 @@
 
 int count_bits_zero(int x){
 
-	int i, total = 0;
+	int i, total = 0; /*Inicialização da variavel total e criaçaão de i*/
 
 	for(i=0; i<32; i++){
 
-		if(x % 2  == 0){
+		if(x % 2  == 0){ /*Condiciona o seguinte, sendo a condição o bit atual ser 0, bit menos significativo determina par ou impar */
 	
-			total++;
+			total++; /*Incrementa total */
 
 		}
 	
-		x = x >> 1;
+		x = x >> 1; /*Shift de x 1 casa para a direita, o bit menos significativo passa a ser o bit anteriormente à sua esquerda */
 	
 	}
 	
--- modulo4/ex10b/count_bits_zero.s
+++ modulo4/ex10b/count_bits_zero.s
@@ -11,17 +11,17 @@ count_bits_zero:
 	pushl %ebp
 	movl %esp, %ebp
 
-	movl 8(%ebp), %eax
-	movl $32, %ecx
-	movl $0, %edx
+	movl 8(%ebp), %eax #Move o primeiro parametro da função da stack para o registo eax
+	movl $32, %ecx #Passa o valor 32 para ecx
+	movl $0, %edx #Passa o valor 0 para edx, é o contador de 0's
 
 	count:
 
-	shr %eax
+	shr %eax #Shift à direita do numero, o bit menos significativo passa para a flag carry
 
-	jc skip
+	jc skip #Se a flag carry for 1 edx não é incrementado
 	
-	incl %edx
+	incl %edx #Incrementa edx
 	
 	skip:
 
